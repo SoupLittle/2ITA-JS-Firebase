be2a078dc72202572b399b397bc8108f58148703
SoupLittle <marleneturlid@gmail.com>
2024-03-06 09:11:03 +0100

    *

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import{ initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -47,6 +48,7 @@
         alert("Data stored successfully")
     })
     .catch((error)=>{
+        console.error("Error:", error)
         alert("Unsuccessful, error"+error)
     })
   }
